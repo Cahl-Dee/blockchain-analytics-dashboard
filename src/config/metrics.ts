4c07da50951415c0d3ce674d5a1ca3471ab1a609
CahlDee <carldiclementi@gmail.com>
2024-12-26 16:36:36 -0600

    clarify that fees are in ETH for now... till we can process and add USD value

--- src/config/metrics.ts
+++ src/config/metrics.ts
@@ -3,8 +3,8 @@ export const metrics = [
   { id: "numActiveAddresses", name: "Active Addresses" },
   { id: "numTransactions", name: "Number of Transactions" },
   { id: "tps", name: "Transactions per Second" },
-  { id: "avgTxFee", name: "Average Transaction Fee" },
-  { id: "totalFees", name: "Total Fees" },
-  { id: "avgBlockFees", name: "Average Block Fees" },
+  { id: "avgTxFee", name: "Average Transaction Fee (ETH)" },
+  { id: "totalFees", name: "Total Fees (ETH)" },
+  { id: "avgBlockFees", name: "Average Block Fees (ETH)" },
   { id: "avgBlockTime", name: "Average Block Time" },
 ];
