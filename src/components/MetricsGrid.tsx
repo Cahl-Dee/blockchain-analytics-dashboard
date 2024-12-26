8b5f9ed10fedfe3e26b9429e2fe4fdae54e60613
CahlDee <carldiclementi@gmail.com>
2024-12-26 16:33:09 -0600

    added support for 2 new chains

--- src/components/MetricsGrid.tsx
+++ src/components/MetricsGrid.tsx
@@ -1,9 +1,9 @@
 export const metrics = [
   { id: "tps", name: "Transactions per Second" },
   { id: "numTransactions", name: "Number of Transactions" },
-  { id: "avgTxFee", name: "Average Transaction Fee" },
-  { id: "totalFees", name: "Total Fees" },
-  { id: "avgBlockFees", name: "Average Block Fees" },
+  { id: "avgTxFee", name: "Average Transaction Fee (ETH)" },
+  { id: "totalFees", name: "Total Fees (ETH)" },
+  { id: "avgBlockFees", name: "Average Block Fees (ETH)" },
   { id: "numContractDeployments", name: "Contract Deployments" },
   { id: "numActiveAddresses", name: "Active Addresses" },
   { id: "avgBlockTime", name: "Average Block Time" },
--- src/config/chains.ts
+++ src/config/chains.ts
@@ -8,8 +8,8 @@ export interface Chain {
 export const chains: Chain[] = [
   { id: "eth", name: "Ethereum", enabled: true, color: "#627EEA" },
   { id: "base", name: "Base", enabled: true, color: "#0052FF" },
-  { id: "op", name: "Optimism", enabled: false, color: "#FF0420" },
-  { id: "arb", name: "Arbitrum One", enabled: false, color: "#12AAFF" },
+  { id: "op", name: "Optimism", enabled: true, color: "#FF0420" },
+  { id: "arb", name: "Arbitrum One", enabled: true, color: "#12AAFF" },
   { id: "poly", name: "Polygon PoS", enabled: false, color: "#8247E5" },
   { id: "bsc", name: "BNB Smart Chain", enabled: false, color: "#F0B90B" },
 ];
