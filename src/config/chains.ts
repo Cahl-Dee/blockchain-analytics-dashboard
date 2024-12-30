373e3d9455b7ded853fc8807a8320f974b0c5897
CahlDee <carldiclementi@gmail.com>
2024-12-30 09:27:57 -0600

    add new chains

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -10,6 +10,9 @@ export const chains: Chain[] = [
   { id: "base", name: "Base", enabled: true, color: "#0052FF" },
   { id: "op", name: "Optimism", enabled: true, color: "#FF0420" },
   { id: "arb", name: "Arbitrum One", enabled: true, color: "#12AAFF" },
-  { id: "poly", name: "Polygon PoS", enabled: false, color: "#8247E5" },
-  { id: "bsc", name: "BNB Smart Chain", enabled: false, color: "#F0B90B" },
+  { id: "nova", name: "Arbitrum Nova", enabled: false, color: "#e1883b" },
+  { id: "zora", name: "Zora", enabled: false, color: "#4c7ef5" },
+  { id: "zksync", name: "zkSync Era", enabled: false, color: "#8c8dfc" },
+  { id: "scroll", name: "Scroll", enabled: false, color: "#10151b" },
+  { id: "blast", name: "Blast", enabled: false, color: "#9ba885" },
 ];
