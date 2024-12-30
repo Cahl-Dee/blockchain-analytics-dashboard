e4ba95f845112d67358630e94403d757a69b155b
CahlDee <carldiclementi@gmail.com>
2024-12-30 09:28:16 -0600

    rename Optimism

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -8,7 +8,7 @@ export interface Chain {
 export const chains: Chain[] = [
   { id: "eth", name: "Ethereum", enabled: true, color: "#627EEA" },
   { id: "base", name: "Base", enabled: true, color: "#0052FF" },
-  { id: "op", name: "Optimism", enabled: true, color: "#FF0420" },
+  { id: "op", name: "OP Mainnet", enabled: true, color: "#FF0420" },
   { id: "arb", name: "Arbitrum One", enabled: true, color: "#12AAFF" },
   { id: "nova", name: "Arbitrum Nova", enabled: false, color: "#e1883b" },
   { id: "zora", name: "Zora", enabled: false, color: "#4c7ef5" },
